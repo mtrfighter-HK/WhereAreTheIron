44f420e1fe67e905020160da39bd667855e7635e
mtrfighter-HK <mtr.fighter@gmail.com>
2026-05-14 00:34:40 +0800

    Update line_config.py

--- 數據儲存檔/line_config.py
+++ 數據儲存檔/line_config.py
@@ -1,38 +1,61 @@
 # line_config.py
 
-# 定義各條路線的車站順序
-# 格式為 { "路線代碼": ["車站1", "車站2", ...] }
+# 定義全港 MTR 路線的物理區間 (Physical Segments)
+# 格式：{"路線代碼": [ (站A, 站B), (站B, 站C), ... ]}
 
-MTR_LINES = {
-    "TWL": ["CEN", "ADM", "TST", "JOR", "YMT", "MOK", "PRE", "SSP", "CSW", "LCK", "MEF", "LAK", "KWF", "KWH", "TWH", "TSW"],
-
-    "ISL": ["KET", "HKU", "SYP", "SHW", "CEN", "ADM", "WAC", "CAB", "TIH", "FOH", "NOP", "QUB", "TAK", "SWH", "SKW", "HFC", "CHW"],
-
-    "KTL": ["WHA", "HOM", "YMT", "MKK", "PRE", "SKM", "KOT", "LOF", "WTS", "DIH", "CHH", "KOB", "NTK", "KWT", "LAT", "YAT", "TIK"],
-
-    "SIL": ["ADM", "OCP", "WCH", "LET", "SOH"],
-
-    "DRL": ["SUN", "DIS"],
-
-    "TCL": ["HOK", "KOW", "OLY", "NAC", "LAK", "TSY", "SUN", "TUC"],
-
-    "AEL": ["HOK", "KOW", "TSY", "AIR", "AWE"],
-
-    "TKL": ["NOP", "QUB", "YAT", "TIK", "TKO", "LHP"],
-
-    "EAL": ["ADM", "EXC", "HUH", "MKK", "KOT", "TAW", "SHT", "FOT", "UNI", "TAI", "NOP", "QUB", "TAK", "SWH", "SKW", "HFC", "CHW"],
-
-    "TML": ["KET", "HKU", "SYP", "SHW", "CEN", "ADM", "WAC", "CAB", "TIH", "FOH", "NOP", "QUB", "TAK", "SWH", "SKW", "HFC", "CHW"],
-
+MTR_SEGMENTS = {
+    "TWL": [ # 荃灣綫
+        ("CEN", "ADM"), ("ADM", "TST"), ("TST", "JOR"), ("JOR", "YMT"),
+        ("YMT", "MOK"), ("MOK", "PRE"), ("PRE", "SSP"), ("SSP", "CSW"),
+        ("CSW", "LCK"), ("LCK", "MEF"), ("MEF", "LAK"), ("LAK", "KWF"),
+        ("KWF", "KWH"), ("KWH", "TWH"), ("TWH", "TSW")
+    ],
+    "ISL": [ # 港島綫
+        ("KET", "HKU"), ("HKU", "SYP"), ("SYP", "SHW"), ("SHW", "CEN"),
+        ("CEN", "ADM"), ("ADM", "WAC"), ("WAC", "CWB"), ("CWB", "TIH"),
+        ("TIH", "FOH"), ("FOH", "NOP"), ("NOP", "QUB"), ("QUB", "TAK"),
+        ("TAK", "SWH"), ("SWH", "SKW"), ("SKW", "HFC"), ("HFC", "CHW")
+    ],
+    "KTL": [ # 觀塘綫
+        ("WHA", "HOM"), ("HOM", "YMT"), ("YMT", "MOK"), ("MOK", "PRE"),
+        ("PRE", "KOT"), ("KOT", "LOF"), ("LOF", "WTS"), ("WTS", "CHH"),
+        ("CHH", "KBD"), ("KBD", "NTK"), ("NTK", "KOW"), ("KOW", "LAT"),
+        ("LAT", "YAT"), ("YAT", "TIK")
+    ],
+    "TKL": [ # 將軍澳綫 (含分支)
+        ("NOP", "QUB"), ("QUB", "YAT"), ("YAT", "TIK"), ("TIK", "TKO"),
+        ("TKO", "HAH"), ("HAH", "POA"), # 往寶琳分支
+        ("TKO", "LHP")                   # 往康城分支
+    ],
+    "TCL": [ # 東涌綫
+        ("HOK", "KOW"), ("KOW", "OLY"), ("OLY", "NAC"), ("NAC", "LAK"),
+        ("LAK", "TSY"), ("TSY", "SUN"), ("SUN", "TUC")
+    ],
+    "AEL": [ # 機場快綫
+        ("HOK", "KOW"), ("KOW", "TSY"), ("TSY", "AIR"), ("AIR", "AWE")
+    ],
+    "EAL": [ # 東鐵綫 (含分支)
+        ("ADM", "EXC"), ("EXC", "HUH"), ("HUH", "MKK"), ("MKK", "KOT"),
+        ("KOT", "TAW"), ("TAW", "SHT"), ("SHT", "FOT"), ("FOT", "RAC"), # RAC 是火炭/馬場，視乎當日運行
+        ("RAC", "UNI"), ("UNI", "TAP"), ("TAP", "TWO"), ("TWO", "FAN"),
+        ("FAN", "SHS"),
+        ("SHS", "LOW"), # 往羅湖分支
+        ("SHS", "LMC")  # 往落馬洲分支
+    ],
+    "TML": [ # 屯馬綫
+        ("WKS", "MOS"), ("MOS", "HEO"), ("HEO", "TSH"), ("TSH", "SHM"),
+        ("SHM", "CIO"), ("CIO", "STW"), ("STW", "CKW"), ("CKW", "TAW"),
+        ("TAW", "HIK"), ("HIK", "DIH"), ("DIH", "TKW"), ("TKW", "SUW"),
+        ("SUW", "KAT"), ("KAT", "HOM"), ("HOM", "HUH"), ("HUH", "ETS"),
+        ("ETS", "AUS"), ("AUS", "NAC"), ("NAC", "MEF"), ("MEF", "TWW"),
+        ("TWW", "KSR"), ("KSR", "YUL"), ("YUL", "LOP"), ("LOP", "TUM")
+    ]
 }
 
 def get_segments(line_code):
-    """
-    自動根據車站列表生成『區間』(Segments)
-    例如輸入 [A, B, C] 會輸出 [(A, B), (B, C)]
-    """
-    stations = MTR_LINES.get(line_code, [])
-    segments = []
-    for i in range(len(stations) - 1):
-        segments.append((stations[i], stations[i+1]))
-    return segments
+    """回傳指定路線的區間清單"""
+    return MTR_SEGMENTS.get(line_code, [])
+
+def get_all_line_codes():
+    """回傳所有已定義的路線代碼"""
+    return list(MTR_SEGMENTS.keys())
