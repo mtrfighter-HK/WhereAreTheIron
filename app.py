d9e36822a02e4f4ea712ec0a75f532bea5a3ae73
mtrfighter-HK <mtr.fighter@gmail.com>
2026-06-10 09:55:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def home():
     return render_template('index.html', total=total)
 
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
